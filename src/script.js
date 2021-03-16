7146d4e8a313d37b2a58bac1937447dbf0a3f932
orangewp <contact@orangewp.com>
2021-03-16 20:24:52 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 10,
+  slidesPerView: 2,
+  slidesPerView: "auto",
+});
+
+let mobileMenu = document.getElementById("mobileMenu");
+let menu = document.getElementById("menu");
+menu.classList.remove("hidden");
+let hamMenuLine1 = document.getElementById("ham-menu-line-1");
+let hamMenuLine2 = document.getElementById("ham-menu-line-2");
+let hamMenuLine3 = document.getElementById("ham-menu-line-3");
+mobileMenu.addEventListener("click", function () {
+  mobileMenu.classList.toggle("fixed");
+  hamMenuLine2.classList.toggle("hidden");
+  hamMenuLine3.classList.toggle("w-3/5");
+  hamMenuLine3.classList.toggle("mt-1");
+  hamMenuLine1.classList.toggle("rotate-45");
+  hamMenuLine3.classList.toggle("-rotate-45");
+  menu.classList.toggle("-translate-y-full");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
